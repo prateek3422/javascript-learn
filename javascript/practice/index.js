d149a95f10af07b30b7dd688570a1bcd054d161e
Prateek Singh <ps0431299#gmail.com>
2023-11-17 20:56:11 +0530

    JavaScript fundamental

--- javascript/practice/index.js
+++ javascript/practice/index.js
@@ -1586,4 +1586,26 @@
 // }
 // console.log(test77(8))
 
-// Q92 
+// Q92 Write a JavaScript program to check whether a matrix is a diagonal matrix or not. In linear algebra, a diagonal matrix is a matrix in which the entries outside the main diagonal are all zero (the diagonal from the upper left to the lower right).
+
+// Example:
+// [1, 0, 0], [0, 2, 0], [0, 0, 3] ]) = true
+// [1, 0, 0], [0, 2, 3], [0, 0, 3] ]) = false
+
+// function test78(a) {
+
+//     for(let i=0; i<a.length; i++){
+//         for(let j=0; j<a.length; j++){
+//             console.log(a[i][j])            
+//             if(i!==j && a[i][j] !==0){
+//                 return false
+//             }
+//         }
+//     }
+//     return true
+
+// }
+// console.log(test78([[1,0,0], [0,1,0], [0,0,1]]))
+// console.log(test78([[1,0,0], [0,1,0], [0,1,1]]))
+
+
